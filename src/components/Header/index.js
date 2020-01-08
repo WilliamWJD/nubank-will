6567071ec1b88d491af0635018efffab1449793b
williamwjd <william.jose.wjd@gmail.com>
2020-01-08 11:22:33 -0300

    Estrutura e estilização do Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-import {Container} from './styles'
+import {Container, Top, Logo, Title} from './styles'
+
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import logo from '~/assets/Nubank_Logo.png'
 
 export default function Header(){
   return(
-    <Container/>
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>William</Title>
+      </Top>
+      <Icon name='keyboard-arrow-down' size={20} color='#fff'/>
+    </Container>
   )
 }
